a24512bd088dede98541f237d7f7ef71f493b48b
ncoder <bhargavayash2015@gmail.com>
2024-06-27 16:28:54 +0530

    added types

--- src/app/user/types.ts
+++ src/app/user/types.ts
@@ -1,11 +1,31 @@
 export const types = `
-type User {
-    id: ID!
-    email: String!
-    password: String!
-    name: String!
-    role: String!
-    createdAt: String!
-    updatedAt: String!
-}
+    type User {
+        id: ID!
+        email: String!
+        password: String!
+        name: String!
+        role: String!
+        Orders: [Order] 
+        createdAt: String!
+        updatedAt: String!
+    }
+    
+    type Order {
+        id: ID!
+        user: User!
+        total: Float!
+        items: [Item]!
+        createdAt: String!
+        updatedAt: String!
+    }
+
+    type Item {
+        id: ID!
+        name: String!
+        price: Float!
+        quantity: Int!
+        order: Order!
+        createdAt: String!
+        updatedAt: String!
+    }
 `
